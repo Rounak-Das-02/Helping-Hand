c070e86b2bb81b4073db12b997e164a8c46f19f6
Rounak <62498036+Rounak-Das-02@users.noreply.github.com>
2022-08-11 23:59:39 +0530

    Deleted Firebase Configs

--- src/firebaseConfig.tsx
+++ src/firebaseConfig.tsx
@@ -1,15 +1,15 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from "firebase";
 const config = {
-  apiKey: "AIzaSyCqaoKJSNdJ5OyEYHiFnJnC8i9_e_qxfac",
-  authDomain: "solution-2022.firebaseapp.com",
+  apiKey: "",
+  authDomain: "",
   databaseURL:
-    "https://solution-2022-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "solution-2022",
-  storageBucket: "solution-2022.appspot.com",
-  messagingSenderId: "762959308956",
-  appId: "1:762959308956:web:ec1cfc866a1410577e6559",
-  measurementId: "G-2LSC5HY178",
+    "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const myApp = firebase.initializeApp(config);
